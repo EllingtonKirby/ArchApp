4e8cb2cdc3b1e6cb511cf21ac4d1461352180e8c
ellingtonkirby <ellingtonkirby31@gmail.com>
2019-11-12 15:03:20 -0500

    launch activity

---
+++ app/src/main/java/com/ellington/architecture/LaunchActivity.kt
@@ -0,0 +1,11 @@
+package com.ellington.architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
