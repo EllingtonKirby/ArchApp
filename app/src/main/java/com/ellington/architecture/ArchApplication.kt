1591d02ca3915cf0921ca334c162cd3fb9d16525
ellingtonkirby <ellingtonkirby31@gmail.com>
2019-12-03 17:30:03 -0500

    Fixes for api 19

--- app/src/main/java/com/ellington/architecture/ArchApplication.kt
+++ app/src/main/java/com/ellington/architecture/ArchApplication.kt
@@ -2,9 +2,14 @@ package com.ellington.architecture
 
 import android.app.Activity
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import com.ellington.dagger.modules.DaggerCoreComponent
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.security.ProviderInstaller
 import kotlin.random.Random
 
 open class ArchApplication : MultiDexApplication() {
@@ -22,7 +27,19 @@ open class ArchApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
-        // Random nightMode to make developer aware of day/night themes
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            try {
+                ProviderInstaller.installIfNeeded(this)
+            } catch (e: GooglePlayServicesRepairableException) {
+                GoogleApiAvailability.getInstance()
+                    .showErrorNotification(this, e.connectionStatusCode)
+                return
+            } catch (e: GooglePlayServicesNotAvailableException) {
+                return
+            }
+        }
+
+        // Random nightMode
         val nightMode = when (Random.nextBoolean()) {
             true -> AppCompatDelegate.MODE_NIGHT_YES
             false -> AppCompatDelegate.MODE_NIGHT_NO
