9c88627cbbaf2e25dfcf2abcdc38100bf5804db1
ellingtonkirby <ellingtonkirby31@gmail.com>
2019-12-03 14:37:11 -0500

    Switch from Rxjava networking to Kotlin suspend coroutines

--- home/src/main/java/com/ellington/home/data/api/AlbumsApi.kt
+++ home/src/main/java/com/ellington/home/data/api/AlbumsApi.kt
@@ -2,19 +2,18 @@ package com.ellington.home.data.api
 
 import com.ellington.home.data.Albums
 import com.ellington.home.data.TrackList
-import io.reactivex.Flowable
-import retrofit2.adapter.rxjava2.Result
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Url
 
 interface AlbumsApi {
     @GET("/2.0/user/{user_id}/albums")
-    fun getAlbums(@Path("user_id") userId: String): Flowable<Result<Albums>>
+    suspend fun getAlbums(@Path("user_id") userId: String): Response<Albums>
 
     @GET
-    fun getNextPageOfAlbums(@Url url: String): Flowable<Result<Albums>>
+    suspend fun getNextPageOfAlbums(@Url url: String): Response<Albums>
 
     @GET
-    fun getTrackList(@Url url: String): Flowable<Result<TrackList>>
+    suspend fun getTrackList(@Url url: String): Response<TrackList>
 }
--- home/src/main/java/com/ellington/home/data/source/impl/AlbumsDataSourceImpl.kt
+++ home/src/main/java/com/ellington/home/data/source/impl/AlbumsDataSourceImpl.kt
@@ -1,65 +1,39 @@
 package com.ellington.home.data.source.impl
 
-import android.util.Log
 import com.ellington.home.data.Album
 import com.ellington.home.data.Albums
 import com.ellington.home.data.TrackList
 import com.ellington.home.data.api.AlbumsApi
 import com.ellington.home.data.source.AlbumsDataSource
 import com.ellington.mvvm.repository.Result
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class AlbumsDataSourceImpl @Inject constructor(private val api: AlbumsApi) : AlbumsDataSource {
     override suspend fun getAlbums(userId: String): Result<Albums> {
-        return api.getAlbums(userId)
-            .subscribeOn(Schedulers.io())
-            .doOnError {
-                Log.e("Error", it.message)
-            }
-            .map {
-                if (!it.isError) {
-                    return@map Result.Success(it.response()?.body() ?: Albums())
-                } else {
-                    return@map Result.Error(Exception(it.error()))
-                }
-            }
-            .onErrorReturn { Result.Error(Exception()) }
-            .blockingLast()
+        val result = api.getAlbums(userId)
+        return if (result.isSuccessful) {
+            Result.Success(result.body() ?: Albums())
+        } else {
+            Result.Error(Exception(result.message()))
+        }
     }
 
     override suspend fun getNextPageOfAlbums(url: String): Result<Albums> {
-        return api.getNextPageOfAlbums(url)
-            .subscribeOn(Schedulers.io())
-            .doOnError { Log.e("Error", it.message) }
-            .map {
-                if (!it.isError) {
-                    return@map Result.Success(it.response()?.body() ?: Albums())
-                } else {
-                    return@map Result.Error(Exception(it.error()))
-                }
-            }
-            .onErrorReturn {
-                Result.Error(Exception())
-            }
-            .blockingLast()
+        val result = api.getNextPageOfAlbums(url)
+        return if (result.isSuccessful) {
+            Result.Success(result.body() ?: Albums())
+        } else {
+            Result.Error(Exception(result.message()))
+        }
     }
 
     override suspend fun getTrackList(url: String): Result<TrackList> {
-        return api.getTrackList(url)
-            .subscribeOn(Schedulers.io())
-            .doOnError { Log.e("Error", it.message) }
-            .map {
-                if (!it.isError) {
-                    return@map Result.Success(it.response()?.body() ?: TrackList())
-                } else {
-                    return@map Result.Error(Exception(it.error()))
-                }
-            }
-            .onErrorReturn {
-                Result.Error(Exception())
-            }
-            .blockingLast()
+        val result = api.getTrackList(url)
+        return if (result.isSuccessful) {
+            Result.Success(result.body() ?: TrackList())
+        } else {
+            Result.Error(Exception(result.message()))
+        }
     }
 
     override suspend fun getAlbumById(albumId: String): Result<Album> {
